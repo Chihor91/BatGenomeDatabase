ade228b58c1f6577e6eab6fa3efc4a1673ce3314
ngarcia <ngarcia@up.edu.ph>
2023-07-19 09:57:44 +0800

    Update imports

--- User/models.py
+++ User/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.base_user import BaseUserManager
 import os
 
 class AccountManager(BaseUserManager):
